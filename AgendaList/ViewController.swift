ab99480bba5b0f471591b6b90ace83d859ae9cf4
josue rangel <josrangel@hotmail.com>
2020-10-21 09:19:18 -0500

    Initial Commit

---
+++ AgendaList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgendaList
+//
+//  Created by KMMX on 21/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
